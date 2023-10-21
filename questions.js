6241bd6f83c917104487f043486c47cdf8e44fe0
Ahmed mohamed salah <101955927+ahmed200salah@users.noreply.github.com>
2023-10-21 23:03:15 +0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,82 +1,68 @@
   let questions = [
     {
     numb: 1,
-    question: "..... is essential not only for business concerns but also for banks.schools,colleges,etc..",
-    answer: "Management",
+    question: "دورة التنفيز تقوم بتنفيز التعليمه بوسطة وحدة التحكم",
+    answer: "خطأ",
     options: [
-      "Management",
-      "objectives",
-      "subjects",
-      "si"
+      "صح",
+      "خطأ",
+      "",
+      ""
     ]
   },
-    {
-    numb: 2,
-    question: "..... has started in his famous book {The Practice of Management} that, the emergence of Management as an essential, a distinct and leading social institution is a pivotal event in social history",
-    answer: "Peter F.Drucker",
+  {    numb: 2,
+    question: "تقوم بتخزين ناتج العملية الحسابيه وارسالها الي الزاكرة الرئيسيه",
+    answer: "دورة التنفيز",
     options: [
-      "Peter F.Drucker",
-      "Ordway tead",
-      "ralph C.Davis",
-      "Devid messi"
+      "وحدة الحساب والمنطق",
+      "دورة التعليمة",
+      "دورة التنفيز",
+      "جميع ما سبق"
     ]
   },
     {
     numb: 3,
-    question: "Management is not people; it is an activity like walking reading, swimming or running. people who perform Management can be designated as members of Management or executive leaders according to .....",
-    answer: "Ordway tead",
+    question: "اول معالج تم تصنيعه للاغراض العامه هو",
+    answer: "لا توجد اجابه صحيحه",
     options: [
-      "ralph C.Davis",
-      "Peter F.Drucker",
-      "Ordway tead",
-      "Devid messi"
+      "8008 year 1974",
+      "8086 year 1978",
+      "8080 year 1978",
+      "لا توجد اجابه صحيحه"
     ]
   },
     {
     numb: 4,
-    question: "Management is a ..... aspect pf the economic life of man, which is an organised group activity.",
-    answer: "vital",
+    question: "مسارات النظام المسار فيها يتكون من عدة اسلاك تسير معا علي التوالي في لوحة النظام وكل سلك يحمل واحد بت في نفس الوقت ",
+    answer: "خطا",
     options: [
-      "Primary",
-      "vital",
-      "third",
-      "secondary"
+      "صح",
+      "خطا",
+      "",
+      ""
     ]
   },
     {
     numb: 5,
-    question: "..... defined Management as an organ, organs can be described and defined only through their functions",
-    answer: "Peter F.Drucker",
+    question: "يتعامل الحاسب مع البيانات رقميه فقط",
+    answer: "صح",
     options: [
-      "Ordway tead",
-      "Peter F.Drucker",
-      "terry",
-      "ralph C.Davis"
-    ]
-  },
-
-    {
-  numb: 6,
-  question: "Management is the sum-total of all those activities that Doesn't provide maximum prosperity and happimess for both employer and employees and public at large?",
-  answer: "False",
-    options: [
-      "True",
-      "False",
+      "صح",
+      "خطا",
       "",
       ""
     ]
   },
 
-
     {
-  numb: 7,
-  question: "Peter F.Drucker has stated in his famous book {..........} that, the emergence of Management as an essential, a distinct and leading social institution is a pivotal event in social history. , ",
-  answer: "The Practice of Management",
+  numb: 6,
+  question: "يقوم الحاسب بمعالجة البيانات ويحولها الي المعلومات",
+  answer: "صح",
     options: [
-      "The Price",
-      "The making of a manager",
-      "Lifelong Learning and Growth",
-      "The Practice of Management"
+      "صح",
+      "خطأ",
+      "",
+      ""
     ]
   },
 ];
